7c6076d1a2c46744ea0b29caf8d1b8441e1a178d
Quentin Caritey <quentin.caritey@neoxia.com>
2021-07-27 16:30:05 +0200

    factory

--- ESGI.DesignPattern.Projet/DescriptorMapper.cs
+++ ESGI.DesignPattern.Projet/DescriptorMapper.cs
@@ -8,12 +8,25 @@ namespace ESGI.DesignPattern.Projet
         protected List<AttributeDescriptor> CreateAttributeDescriptors() {
             var result = new List<AttributeDescriptor>();
 
-            result.Add(new DefaultDescriptor("remoteId", GetClass(), typeof(int)));
-            result.Add(new DefaultDescriptor("createdDate", GetClass(), typeof(DateTime)));
-            result.Add(new DefaultDescriptor("lastChangedDate", GetClass(), typeof(DateTime)));
-            result.Add(new ReferenceDescriptor("createdBy", GetClass(), typeof(User)));
-            result.Add(new ReferenceDescriptor("lastChangedBy", GetClass(), typeof(User)));
-            result.Add(new DefaultDescriptor("optimisticLockVersion", GetClass(), typeof(int)));
+            result.Add(
+                AttributeDescriptor.create("default", "remoteId", GetClass(), typeof(int))
+            );
+            result.Add(
+                AttributeDescriptor.create("default", "createdDate", GetClass(), typeof(DateTime))
+            );
+            result.Add(
+                AttributeDescriptor.create("default", "lastChangedDate", GetClass(), typeof(DateTime))
+            );
+            result.Add(
+                AttributeDescriptor.create("reference", "createdBy", GetClass(), typeof(User))
+            );
+            result.Add(
+                AttributeDescriptor.create("reference", "lastChangedBy", GetClass(), typeof(User))
+            );
+            result.Add(
+                AttributeDescriptor.create("default", "optimisticLockVersion", GetClass(), typeof(int))
+            );
+
             return result;
         }
 
---
+++ ESGI.DesignPattern.Projet/descriptors/AttributeDescriptor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ESGI.DesignPattern.Projet
+{
+    public abstract class AttributeDescriptor
+    {
+        private readonly string descriptorName;
+        private readonly Type mapperType;
+        private readonly Type forType;
+
+        private AttributeDescriptor()
+        {}
+
+        public static AttributeDescriptor Create(string config, string descriptorName, Type mapperType, Type forType)
+        {
+            switch(config)
+            {
+                case "boolean":
+                    return new BooleanDescriptor(descriptorName, mapperType, forType);
+                case "reference":
+                    return new ReferenceDescriptor(descriptorName, mapperType, forType);
+                case "default":
+                    return new DefaultDescriptor(descriptorName, mapperType, forType);
+                default:
+                    throw new Exception("unknown config");
+            }
+        }
+
+        protected AttributeDescriptor(string descriptorName, Type mapperType, Type forType)
+        {
+            this.descriptorName = descriptorName;
+            this.mapperType = mapperType;
+            this.forType = forType;
+        }
+
+        public string DescriptorName => descriptorName;
+    }
+}
